a5d48906eb9b526b4952bf2567c111c904597579
friendship41 <stage41@nate.com>
2020-11-06 10:50:25 +0900

    token response data 수정

--- src/main/kotlin/com/friendship41/authserver/data/ResponseData.kt
+++ src/main/kotlin/com/friendship41/authserver/data/ResponseData.kt
@@ -1,10 +1,10 @@
 package com.friendship41.authserver.data
 
 data class ResBodyOauthToken(
-        var accessToken: String,
-        var tokenType: String,
-        var refreshToken: String,
-        var expiresIn: Long,
+        var access_token: String,
+        var token_type: String,
+        var refresh_token: String,
+        var expires_in: Long,
         var roles: String
 )
 
