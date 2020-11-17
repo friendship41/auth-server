00394440a38b640c753b300c03ef3ba88bdec1e6
friendship41 <stage41@nate.com>
2020-11-17 21:23:44 +0900

    회원가입시 패스워드 인코딩

--- src/main/kotlin/com/friendship41/authserver/handler/MemberAuthHandler.kt
+++ src/main/kotlin/com/friendship41/authserver/handler/MemberAuthHandler.kt
@@ -3,6 +3,7 @@ package com.friendship41.authserver.handler
 import com.friendship41.authserver.data.MemberAuthInfo
 import com.friendship41.authserver.data.MemberAuthInfoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -11,9 +12,11 @@ import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Mono
 
 @Component
-class MemberAuthHandler(@Autowired private val memberAuthInfoRepository: MemberAuthInfoRepository) {
+class MemberAuthHandler(@Autowired private val memberAuthInfoRepository: MemberAuthInfoRepository,
+                        @Autowired private val passwordEncoder: PasswordEncoder) {
     fun handlePostAuthinfo(request: ServerRequest): Mono<ServerResponse> = ok().body(
             request.bodyToMono(MemberAuthInfo::class.java)
                     .map {
+                        it.memberPassword = passwordEncoder.encode(it.memberPassword)
                         memberAuthInfoRepository.save(it) }
                     .doOnNext { it.memberPassword="" })}
